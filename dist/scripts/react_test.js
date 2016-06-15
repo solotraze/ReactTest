8c7c55b91b42b718d723303291420438072b7498
Solo Traze Dev <traze.solo.dev@gmail.com>
2016-06-16 01:26:28 +0530

    basic binding

--- dist/scripts/react_test.js
+++ dist/scripts/react_test.js
@@ -1,13 +1,25 @@
-var CommentBox = React.createClass({
+var Comment = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+      <div className="comment">
+      {this.props.author}: {this.props.children}
       </div>
     );
   }
 });
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, {this.props.guest}! Comment List follows.
+        <Comment author="user one">This is the first comment.</Comment>
+        <Comment author="user two">This is the second comment.</Comment>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <CommentBox />,
+  <CommentList guest="solo"/>,
   document.getElementById('content')
 );
