fcc29180c96348f7e7847b906fd34b4f97a5628d
Solo Traze <solo@deb7.solohome>
2016-06-23 19:12:52 +0530

    Basic dummy test.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('AddFunc', function() {
+  describe('AddNum', function() {
+    it('should add two numbers correctly', function() {
+      assert.equal(5, (3+2));
+    });
+  });
+});
